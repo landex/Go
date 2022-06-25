7822c9003b9891259c067346f3918ef39a448597
Landi Leite <landileite1@gmail.com>
2022-06-25 18:51:23 -0300

    Head First Go 1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/landex/Go
+
+go 1.18
---
+++ hello_go.go
@@ -0,0 +1,13 @@
+// This is my go program
+
+package main //|-> This line says all the rest of code in this file belongs to the "main" package
+
+// |-> This says we'll be using text-formatting code from the "fmt" package
+import "fmt"
+
+// |-> The "main" function is special; it gets run first when your program runs
+func main() {
+
+	fmt.Println("This is my first GO program in Windows!!!") //|-> This line displays("prints") in your terminal
+	//|-> It dows this by calling the "Println" function from the "fmt" package
+}
