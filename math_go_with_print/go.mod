6d0443ef4fae68362b0e83240f71dbaf925aaeec
Landi Leite <landileite1@gmail.com>
2022-06-26 14:14:17 -0300

    Test

---
+++ math_go_with_print/go.mod
@@ -0,0 +1,5 @@
+module github.com/landex/Go
+
+go 1.18
+
+require golang.org/x/text v0.3.7 // indirect
---
+++ math_go_with_print/math_go_print.go
@@ -0,0 +1,14 @@
+// In This program we will combine the math and strings in same programm
+
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+func main() {
+	fmt.Println(math.Floor(2.75))
+	fmt.Println(strings.Title("head first go"))
+}
